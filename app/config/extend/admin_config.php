239ba4ae547d48a65815ced6b9487b32bbc5af68
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-13 00:07:20 +0700

    vercel

---
+++ app/config/extend/admin_config.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: 713uk13m
+ * Date: 6/8/18
+ * Time: 11:39
+ */
+$config['admin_config_basic_auth'] = [
+    'serviceId' => 'CodeIgniter skeleton on Vercel',
+    'framework' => 'CodeIgniter v' . CI_VERSION,
+    'username' => 'hungna',
+    'password' => '20240908151234_3081d5b916d94eda017a273d720e116f1e622d67'
+];
